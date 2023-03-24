df2d3b90113c967a78e9e4bc7ce965993aa771f6
Alex Riasanovsky <ariasanovsky@users.noreply.github.com>
2023-03-24 19:23:50 -0400

    init commit

---
+++ src/colored_graph.rs
@@ -0,0 +1,168 @@
+pub use crate::r_3_3::*;
+
+use bit_fiddler::{set, unset, is_set, mask};
+use rand::prelude::*;
+use bit_iter::BitIter;
+
+pub type Color = usize;
+pub type Vertex = usize;
+pub type Edge = (Vertex, Vertex);
+
+pub struct ColoredEdge {
+    pub color: Color,
+    pub edge: Edge
+}
+pub struct Recoloring {
+    pub old_color: Color,
+    pub new_color: Color,
+    pub edge: Edge
+}
+
+pub const fn choose(n: usize, k: usize) -> usize {
+    match (n, k) {
+        (_, 0) => 1,
+        (0, _) => 0,
+        _ => choose(n-1, k) + choose(n-1, k-1)
+    }
+}
+
+pub const fn choose_two(n: usize) -> usize {
+    n*(n+1)/2-n
+}
+
+#[cfg(test)]
+mod math_tests {
+
+    use super::*;
+
+    #[test]
+    fn choose_two_consistent() {
+        for n in 0..100 {
+            assert_eq!(choose_two(n),
+            choose(n, 2));
+        }
+    }
+}
+
+pub const C: Color = S.len();
+pub const E: usize = choose_two(N);
+pub struct ColoredGraph {
+    neighborhoods: [[Uxx; N]; C]
+}
+
+pub fn random_edge(rng: &mut ThreadRng) -> Edge {
+    let u = rng.gen_range(0..N);
+    let v = rng.gen_range(0..N-1);
+    if v < u { (v, u) } else { (u, v+1) }
+}
+
+impl ColoredGraph {
+    pub fn count_cliques(&self, color: Color, s: Option<usize>, candidates: Option<Uxx>) -> Iyy {
+        let s = s.unwrap_or(S[color]);
+        if s == 0 { return 1 }
+        
+        let candidates = candidates.unwrap_or(mask!([0..N], Uxx));
+        if s == 1 { return candidates.count_ones() as Iyy } 
+        
+        BitIter::from(candidates)
+            .map(|u| candidates & self.bit_neighborhood(color, u) & mask!([u..N], Uxx))
+            .map(|candidates| self.count_cliques(color, Some(s-1), Some(candidates)))
+            .sum()
+    }
+
+    pub fn count_edge_cliques(&self, color: Color, (u, v): Edge) -> Iyy {
+        let candidates = Some(
+            self.common_neighborhood(color, u, v));
+        self.count_cliques(color, Some(S[color]-2), candidates)
+    }
+    
+    pub fn red() -> ColoredGraph {
+        let mut neighborhoods: [[Uxx; N]; C] = [[0; N]; C];
+        for u in 0..N {
+            let mut neighborhood = mask!([0..N], Uxx);
+            unset!(in neighborhood, Uxx, u);
+            neighborhoods[0][u] = neighborhood
+        }
+        ColoredGraph { neighborhoods }
+    }
+
+    fn add(&mut self, c: Color, edge: Edge) {
+        let (u, v) = edge;
+        self.neighborhoods[c][u] = set!((self.neighborhoods[c][u]), Uxx, v);
+        self.neighborhoods[c][v] = set!((self.neighborhoods[c][v]), Uxx, u);
+    }
+
+    fn delete(&mut self, c: Color, edge: Edge) {
+        let (u, v) = edge;
+        self.neighborhoods[c][u] = unset!((self.neighborhoods[c][u]), Uxx, v);
+        self.neighborhoods[c][v] = unset!((self.neighborhoods[c][v]), Uxx, u);
+    }
+
+    pub fn recolor(&mut self, recolor: Recoloring) {
+        self.delete(recolor.old_color, recolor.edge);
+        self.add(recolor.new_color, recolor.edge);
+    }
+
+    pub fn color(&self, (u, v): Edge) -> Option<Color> {
+        match self.neighborhoods
+            .iter()
+            .position(|&neighborhood| 
+            is_set!((neighborhood[u]), Uxx, v)) {
+                Some(c) => Some(c),
+                None => None
+        }
+    }
+
+    fn bit_neighborhood(&self, color: Color, u: Vertex) -> Uxx {
+        self.neighborhoods[color][u]
+    }
+
+    fn common_neighborhood(&self, color: Color, u: Vertex, v: Vertex) -> Uxx {
+        self.bit_neighborhood(color, u) & 
+        self.bit_neighborhood(color, v)
+    }
+
+    pub fn random_edge(&mut self, rng: &mut ThreadRng) -> ColoredEdge {
+        let edge = random_edge(rng);
+        let color = self.color(edge)
+            .unwrap();
+        ColoredEdge { color, edge }
+    }
+
+    pub fn show_matrix(&self) {
+        if C == 0 { println!("Colorless graph!") }
+        for u in 1..N {
+            for v in 0..u {
+                let c = self.color((u, v)).unwrap_or(C);
+                print!("{c}");
+            }
+            print!("; ");
+        }
+    }
+
+    pub fn show_neighborhoods(&self) {
+        for u in 0..N {
+            print!("vertex {u}:");
+            for c in 0..C {
+                print!(" {:?}", BitIter::from(self.bit_neighborhood(c, u)).collect::<Vec<_>>());
+            }
+            println!();
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn only_red_cliques() {
+        let red = ColoredGraph::red();
+        assert_eq!(choose(N, S[0]) as Iyy,
+            red.count_cliques(0, None, None));
+        for c in 1..C {
+            assert_eq!(0,
+                red.count_cliques(c, None, None))
+        }
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+pub mod   n_leq_8 { pub type Uxx =   u8; }
+pub mod  n_leq_16 { pub type Uxx =  u16; }
+pub mod  n_leq_32 { pub type Uxx =  u32; }
+pub mod  n_leq_64 { pub type Uxx =  u64; }
+pub mod n_leq_128 { pub type Uxx = u128; }
+
+pub mod  count_leq_2_31 { pub type Iyy = i32; }
+pub mod  count_leq_2_63 { pub type Iyy = i32; }
+
+pub mod s_3_3 { pub const S: [usize; 2] = [3, 3]; }
+pub mod s_3_4 { pub const S: [usize; 2] = [3, 4]; }
+pub mod s_3_5 { pub const S: [usize; 2] = [3, 5]; }
+pub mod s_4_4 { pub const S: [usize; 2] = [4, 4]; }
+pub mod s_4_5 { pub const S: [usize; 2] = [4, 5]; }
+pub mod s_5_5 { pub const S: [usize; 2] = [5, 5]; }
+
+pub mod s_3_3_3 { pub const S: [usize; 3] = [3, 3, 3]; }
+
+pub mod r_3_3 {
+    pub const N: usize = 5;
+    pub use crate::{n_leq_8::*, count_leq_2_31::*, s_3_3::*};
+}
+
+pub mod r_3_4 {
+    pub const N: usize = 24;
+    pub use crate::{n_leq_32::*, count_leq_2_31::*, s_3_4::*};
+}
+
+pub mod r_3_3_3 {
+    pub const N: usize = 16;
+    pub use crate::{n_leq_16::*, count_leq_2_31::*, s_3_3_3::*};
+}
+
+pub mod colored_graph;
